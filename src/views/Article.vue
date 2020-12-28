582cdeef8c67ffe3cf494866b818bae8ac9f2946
renmu123 <19961017fyx>
2020-12-28 21:01:22 +0800

    init

---
+++ src/views/Article.vue
@@ -0,0 +1,32 @@
+<template>
+  <vue-markdown
+    :toc="true"
+    toc-id="toc"
+    :toc-anchor-link="false"
+    :toc-first-level="1"
+    >{{ message }}</vue-markdown
+  >
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import VueMarkdown from "vue-markdown";
+import data from "@/mock/articles.js";
+
+const Props = Vue.extend({
+  props: {
+    id: String
+  }
+});
+
+@Component({
+  name: "Article",
+  components: { VueMarkdown }
+})
+export default class HelloWorld extends Props {
+  message: string = data[this.id].content;
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <div class="article" v-for="n in 40" :key="n">第{{ n }}个笔记}</div>
+      </el-aside>
+      <el-main class="main" style="height: 95vh"
+        ><Article :id="id"></Article
+      ></el-main>
+    </el-container>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import Article from "@/views/Article.vue";
+import data from "@/mock/mock.js";
+import { v4 as uuidv4 } from "uuid";
+@Component({
+  name: "home",
+  components: { Article }
+})
+export default class HelloWorld extends Vue {
+  nowArticle = "";
+  id = "80336940-715e-4c57-bb03-e08123310869";
+  mounted() {
+    console.log("aaa", uuidv4());
   }
-};
+}
 </script>
+
+<style lang="scss">
+.article {
+  margin: 20px 0px;
+  cursor: pointer;
+}
+.aside {
+  height: 95vh;
+  text-align: center;
+}
+.container {
+  margin-top: 20px;
+}
+</style>
