996334d6e08ae5320d04625aeff59a1690eb7f71
renmu123 <1101022351@qq.com>
2021-02-02 11:40:43 +0800

    初始化数据库文件

--- src/background.ts
+++ src/background.ts
@@ -1,60 +1,54 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, BrowserWindow, ipcMain } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import DataStore from "nedb-promises";
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-// Scheme must be registered before the app is ready
+const dbFactory = (fileName: string) =>
+  DataStore.create({
+    filename: `${app.getAppPath()}/data/db/${fileName}`,
+    timestampData: true,
+    autoload: true
+  });
+
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } }
 ]);
 
 async function createWindow() {
-  // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: (process.env
         .ELECTRON_NODE_INTEGRATION as unknown) as boolean
     }
   });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
     if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
     createProtocol("app");
-    // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
 }
 
-// Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
     try {
       await installExtension(VUEJS_DEVTOOLS);
     } catch (e) {
@@ -62,6 +56,10 @@ app.on("ready", async () => {
     }
   }
   createWindow();
+  dbFactory("user.db");
+  dbFactory("note.db");
+  dbFactory("notebook.db");
+  dbFactory("tag.db");
 });
 
 // Exit cleanly on request from parent process in development mode.
@@ -78,3 +76,9 @@ if (isDevelopment) {
     });
   }
 }
+
+// {token: token, method: 'insert, findOne', dbname: 'notes', params: {username: "life"}};
+ipcMain.on("db-exce", (event, args) => {
+  // event.reply('asynchronous-reply', 'pong')
+  // event.returnValue = 'pong'
+});
